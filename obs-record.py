6277b6d1319cb11a24f040cfb5d99998b76bffab
Jaerock Kwon <jaerock.kwon@gmail.com>
2023-02-06 17:02:15 -0500

    fix a key consts

--- obs-record.py
+++ obs-record.py
@@ -3,8 +3,8 @@ import argparse
 import pyautogui
 
 # config
-record_start_key = 's'
-record_stop_key = 'e'
+record_start_key = b's'
+record_stop_key = b'e'
 record_start_hotkey = 'f3'
 record_stop_hotkey = 'f4'
 baudrate = 115200
@@ -16,7 +16,7 @@ def main(args):
     ser.port = args.port
     ser.open()
 
-    print('Monitoring VICON trigger signal...')
+    print('OBS-RECORD via VICON trigger signals: F3-Record, F4-Stop')
     print('** Make sure your OBS window is focused.')
 
     pyautogui.alert('Click OK when you are ready. Make sure the OBS window to be focused.', 'VICON Trigger Monitor for OBS', button='OK')
@@ -35,7 +35,7 @@ def main(args):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='OBS Recorder: triggered by VICON ver 0.1 by Jaerock Kwon, 2023')
+    parser = argparse.ArgumentParser(description='OBS Recorder: triggered by VICON ver 0.2 by Jaerock Kwon, 2023')
     parser.add_argument("port", type=str, help="Serial port name. COM{#} or /dev/ttyUSB{#}")
     parser.add_argument("-v", "--verbose", type=bool, default=True, help="increase output verbosity")
     parser.add_argument("-b", "--baudrate", type=int, default=115200, choices=[9600, 14400, 19200, 28800, 31250, 38400, 57600, 115200], help="baudrate")
